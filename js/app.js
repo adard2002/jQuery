8ba7dea29e7a2f8fff80b246f4ebd9ba26dc8c23
adard2002 <adard2002@gmail.com>
2021-02-10 19:08:41 -0600

    getting lab 3 kinda done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('script is connected');
 function Horn (img,title,desc,keyword,horns) {
-  this.img = img;
+  this.image_url = img;
   this.title = title;
   this.description = desc;
   this.keyword = keyword;
@@ -9,28 +9,18 @@ function Horn (img,title,desc,keyword,horns) {
 }
 
 Horn.prototype.render = function (){
-
-  const $template = $('#photo-template').clone();
-  $template.removeAttr('id');
-  $template.attr('id',this.keyword);
-  console.log($template.attr('id'));
-
-  const $h2 = $template.find('h2');
-  $h2.text(this.title);
-  console.log('within render function, h2:',this.title);
-  const $image = $template.find('img');
-  $image.attr('src', this.img);
-  $image.attr('alt', this.keyword);
-  $template.find('p').text(this.description);
-  console.log($template);
-  $('main').append($template);
+  const htmlTemplate = $('#photo-template').html();
+  const hornObject = this;
+  const renderedHornObject = Mustache.render(htmlTemplate, hornObject);
+  $('section').append(renderedHornObject);
 };
 
-
+const horns2 = [];
+const horns = [];
 
 $.ajax('data/page-1.json').then(callStuffBack => {
   console.log(callStuffBack);
-  const horns = [];
+
   callStuffBack.forEach( (horner) => {
     horns.push(new Horn(horner.image_url,horner.title,horner.description,horner.keyword,horner.horns));
     $('select').append(`<option value="${horner.keyword}">${horner.keyword}</option>`);
@@ -41,13 +31,41 @@ $.ajax('data/page-1.json').then(callStuffBack => {
   $('select').change( function () {
     const choice = $('select').find(':selected').text();
     console.log(choice);
-    $('section').hide();
-    $(`#${choice}`).show();
+    $('div').hide();
+    $(`.${choice}`).show();
   });
   horns.forEach(horner => { horner.render();});
 });
 
+$.ajax('data/page-2.json').then(callStuffBack => {
+  console.log(callStuffBack);
+  callStuffBack.forEach( (horner) => {
+    horns2.push(new Horn(horner.image_url,horner.title,horner.description,horner.keyword,horner.horns));
+    $('select').append(`<option value="${horner.keyword}">${horner.keyword}</option>`);
+    console.log('json horners:',horner);
+  });
 
+
+  $('select').change( function () {
+    const choice = $('select').find(':selected').text();
+    console.log(choice);
+    $('div').hide();
+    $(`.${choice}`).show();
+  });
+
+});
+function showPage(page) {
+  console.log(`page ${page} clicked`);
+  if (page === 1) {
+    $('section').empty();
+    horns.forEach(horner => { horner.render();});
+  }
+  if (page === 2){
+    $('section').empty();
+    horns2.forEach(horner => { horner.render();});
+  }
+}
+showPage(1);
 
 
 
